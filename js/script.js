8167df5a537714da71e1b64af484f88f431db4c8
Rodrigo Lima <rodrigo.lima.oliveira@outlook.com>
2021-10-20 00:42:30 -0300

    Configurado os demais digitos, e feita a lógica para troca de digito

--- js/script.js
+++ js/script.js
@@ -32,5 +32,40 @@ function main() {
 
 function numeroUm() {
     const element = document.querySelector("#digitos");
-    element.classList.add("um")
+    var palpite = document.getElementById("palpite").value;
+
+    switch(palpite) {
+        case "0":
+            element.classList.add("zero")
+            break;
+        case "1":
+            element.classList.add("um")
+            break;
+        case "2":
+        element.classList.add("dois")
+            break;
+        case "3":
+            element.classList.add("tres")
+            break;
+        case "4":
+            element.classList.add("quatro")
+            break;
+        case "5":
+            element.classList.add("cinco")
+            break;
+        case "6":
+            element.classList.add("seis")
+            break;
+        case "7":
+            element.classList.add("sete")
+            break;
+        case "8":
+            element.classList.add("oito")
+            break;
+        case "9":
+            element.classList.add("nove")
+            break;
+    }
+    
+    
 }
